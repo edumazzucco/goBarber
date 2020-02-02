a4efd375efc7ca670530381f707eb75452a64628
edumazzucco <edumazzucco@hotmail.com>
2020-02-02 14:08:30 -0300

    fix: fixed session token verification

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,4 @@
 import jwt from 'jsonwebtoken';
-
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { promissify } from 'util';
+import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
 
@@ -7,18 +7,17 @@ export default async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-        return res.status(401).json({ error: 'Token not provided ' });
+        return res.status(401).json({ error: 'Token not provided' });
     }
 
     const [, token] = authHeader.split(' ');
 
     try {
-        const decoded = await promissify(jwt.verify)(token, authConfig.secret);
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
         req.userId = decoded.id;
-
         return next();
-    } catch (err) {
-        return res.status(401).json({ error: 'Invalid token' });
+    } catch (error) {
+        return res.status(401).json({ error: 'Token invalid' });
     }
 };
